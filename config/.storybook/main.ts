2d64b6ab749b4747e0c7777efcccb923abf2c97f
Sergey-SM2021 <sergey2003.k.96@gmail.com>
2023-06-25 22:53:39 +0300

    sa

--- config/.storybook/main.ts
+++ config/.storybook/main.ts
@@ -23,16 +23,16 @@ const config: StorybookConfig = {
         path.resolve(__dirname, "../../", "node_modules"),
       ],
       fallback: {
-        fs: false,
-        tls: false,
-        net: false,
-        path: false,
-        zlib: false,
-        http: false,
-        https: false,
-        stream: false,
-        crypto: false,
-        "crypto-browserify": require.resolve("crypto-browserify")
+        url: require.resolve("url"),
+        fs: require.resolve("fs"),
+        assert: require.resolve("assert"),
+        crypto: require.resolve("crypto-browserify"),
+        http: require.resolve("stream-http"),
+        https: require.resolve("https-browserify"),
+        os: require.resolve("os-browserify/browser"),
+        buffer: require.resolve("buffer"),
+        stream: require.resolve("stream-browserify"),
+        "crypto-browserify": require.resolve("crypto-browserify"),
       },
     };
 
